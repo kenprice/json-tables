cd5c9b99c5b05fe700098de9801fe42396760e82
Ken Price <kenaprice@gmail.com>
2017-01-22 17:36:24 -0500

    Add StringHelper methods for parsing ints, bools

--- src/Helpers/StringHelper.php
+++ src/Helpers/StringHelper.php
@@ -11,4 +11,35 @@ class StringHelper
     {
         return preg_match('/^[\w\-]+$/', $str) === 1;
     }
+
+    /**
+     * Parses input to non-negative integer. Null if invalid.
+     * @param string $str
+     * @return mixed
+     */
+    public static function parseIntNonNegative(string $str)
+    {
+        return filter_var(
+            $str,
+            FILTER_VALIDATE_INT,
+            array(
+                "options" => array("min_range" => 0),
+                "flags" => FILTER_NULL_ON_FAILURE)
+        );
+    }
+
+    /**
+     * Parses input to bool. Null if invalid.
+     * Note: Blank string will return false.
+     * @param string $str
+     * @return mixed
+     */
+    public static function parseBool(string $str)
+    {
+        return filter_var(
+                $str,
+                FILTER_VALIDATE_BOOLEAN,
+                FILTER_NULL_ON_FAILURE
+            );
+    }
 }
