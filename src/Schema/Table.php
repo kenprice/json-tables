4bdb68014ae7d969c4197a9bb819f7a013d298e5
Ken Price <kenaprice@gmail.com>
2017-01-22 21:15:07 -0500

    Refactor Table to use notifications instead of throwing exceptions

--- src/Schema/Table.php
+++ src/Schema/Table.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace JsonTables\Schema;
+
 use JsonTables\Exceptions;
+use JsonTables\Notification;
 
 /**
  * Class Table
@@ -17,18 +19,15 @@ class Table
      * Table constructor.
      * @param array $dictTable Array containing name, fields, primaryKeys,
      *                         and foreignKeys
-     * @throws Exceptions\InvalidSchemaException
      */
     public function __construct(array $dictTable)
     {
-        if (!array_key_exists("name", $dictTable)) {
-            throw new Exceptions\InvalidSchemaException('"name" is required.');
+        if (array_key_exists("name", $dictTable)) {
+            $this->name = $dictTable["name"];
         }
-        if (!array_key_exists("fields", $dictTable)) {
-            throw new Exceptions\InvalidSchemaException('"fields" is required.');
+        if (array_key_exists("fields", $dictTable)) {
+            $this->populateFields($dictTable["fields"]);
         }
-        $this->name = $dictTable["name"];
-        $this->populateFields($dictTable["fields"]);
         if (array_key_exists("primaryKey", $dictTable)) {
             $this->primaryKey = $dictTable["primaryKey"];
         }
@@ -40,17 +39,37 @@ class Table
     /**
      * Populates fields with Field objects from table fields in parsed JSON schema
      * @param array $dictFields
-     * @throws Exceptions\InvalidSchemaException
      */
     private function populateFields(array $dictFields)
     {
-        if (!$dictFields) {
-            throw new Exceptions\InvalidSchemaException('"fields" cannot be empty.');
-        }
         $this->fields = [];
         foreach ($dictFields as $dictField) {
             $field = new Field($dictField);
             array_push($this->fields, $field);
         }
+    }
+
+    public function check()
+    {
+        if ($this->validation()->hasErrors()) {
+            throw new Exceptions\InvalidSchemaException(
+                $this->validation()->errorMessages('Invalid Constraints')
+            );
+        }
+    }
+
+    public function validation()
+    {
+        $note = new Notification();
+        if ($this->name === null) {
+            $note->addError('"name" is required.');
+        }
+        if ($this->name !== null && !StringHelper::stringIsAlphaNumDashUnderscore($this->name)) {
+            $note->addError('"name" must contain only alphanumeric characters, dash, or underscore.');
+        }
+        if ($this->fields === null) {
+            $note->addError('"fields" is required.');
+        }
+        return $note;
     }
 }
