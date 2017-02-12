520edef908260500a08f61ea8ead9c877b56ea1c
Ken Price <kenaprice@gmail.com>
2017-02-11 19:40:47 -0500

    Add integer parser to StringHelper

--- src/Helpers/StringHelper.php
+++ src/Helpers/StringHelper.php
@@ -19,6 +19,20 @@ class StringHelper
     }
 
     /**
+     * Parses input to an integer. Null if invalid.
+     * @param string $str
+     * @return mixed
+     */
+    public static function parseInt(string $str)
+    {
+        return filter_var(
+            $str,
+            FILTER_VALIDATE_INT,
+            array("flags" => FILTER_NULL_ON_FAILURE)
+        );
+    }
+
+    /**
      * Parses input to non-negative integer. Null if invalid.
      * @param string $str
      * @return mixed
